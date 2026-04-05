37164f881f9bdaff8db673e11258195f17006bab
Maurice Kastelijn <maurice.kastelijn@philips.com>
2026-04-05 16:16:20 +0200

    Update deployment URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 // Change `base` before publishing to GitHub Pages under a repo path.
 // Example: /my-awesome-game/
 export default defineConfig({
-  base: '/phaser-game-template/',
+  base: '/archipelago-game/',
   build: {
     // Phaser is the dominant client dependency in this template, so the default
     // 500 kB warning is too low to be useful for the initial starter build.
